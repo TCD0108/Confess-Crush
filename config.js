e6760a8baab91709e110dcd9a097f61157ffc3d7
TCD0108 <87407289+TCD0108@users.noreply.github.com>
2021-12-06 18:37:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi em <3",
     introTitle: 'Phạm Thị Hữu Thúy',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Vì em anh như người điên mất trí
+    Vì em anh như chẳng còn biết nghĩ suy
+    Vì anh đã trót lỡ đắm say em k bận tâm mai sau thế nao`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Hk nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho em <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Iu anh hk cần lý do <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hiện tại hay là tương lai em vẫn mãi mãi luôn là của anh nha.',
     btnAccept: 'Okiiiii lun <3',
